1c9d5f73b3b839ac725126bbde907e969abdda6c
Ian Moffett <ian@lunota.org>
2026-01-17 22:04:11 -0500

    hdl: alu: Add bitwise operations
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- hdl/cpu/alu.sv
+++ hdl/cpu/alu.sv
@@ -5,7 +5,10 @@
 
 module alu #(
     parameter ALU_SUB_IMM = 8'h01,
-    parameter ALU_ADD_IMM = 8'h03
+    parameter ALU_ADD_IMM = 8'h03,
+    parameter ALU_OR_IMM  = 8'h08,
+    parameter ALU_AND_IMM = 8'h0A,
+    parameter ALU_XOR_IMM = 8'h0C
 ) (
     input  logic [7:0] operand_0,
     input  logic [7:0] operand_1,
@@ -16,6 +19,9 @@ module alu #(
         case (opcode)
             ALU_ADD_IMM: result = operand_0 + operand_1;
             ALU_SUB_IMM: result = operand_0 - operand_1;
+            ALU_OR_IMM:  result = operand_0 | operand_1;
+            ALU_AND_IMM: result = operand_0 & operand_1;
+            ALU_XOR_IMM: result = operand_0 ^ operand_1;
             default: result = 0;
         endcase
     end
